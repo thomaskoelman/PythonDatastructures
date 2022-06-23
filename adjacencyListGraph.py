e6c3f7495bf27a261f92cc587aedca0f3bc95efc
Thomas Koelman <thomaskoelman@hotmail.com>
2022-06-23 13:08:42 +0200

    Update adjacencyListGraph.py

--- adjacencyListGraph.py
+++ adjacencyListGraph.py
@@ -1,11 +1,12 @@
 class Graph:
-    def __init__(self, directed=False):
+    def __init__(self, directed=False, nodes=0):
         self.__is_directed = directed
         self.__storage = []
         self.__order = 0
         self.__nmbr_of_edges = 0
+        self.__add_node(nodes)
 
-    def add_node(self, nmbr=1):
+    def __add_node(self, nmbr=1):
         for i in range(nmbr):
             self.__storage.append([])
             self.__order = self.__order + 1
@@ -14,10 +15,22 @@ class Graph:
         if (not self.is_adjacent(from_node, to_node)):
             self.__storage[from_node].append(to_node)
             self.__nmbr_of_edges = self.__nmbr_of_edges + 1
-            if (not self.__is_directed):
+            if (not self.is_directed()):
                 self.__storage[to_node].append(from_node)
         else:
             pass
+
+    def delete_edge(self, from_node, to_node):
+        if (self.is_adjacent(from_node, to_node)):
+            self.__storage[from_node].remove(to_node)
+            self.__nmbr_of_edges = self.__nmbr_of_edges - 1
+            if (not self.is_directed()):
+                self.__storage[to_node].remove(from_node)
+        else:
+            pass
+
+    def is_directed(self):
+        return self.__is_directed
 
     def order(self):
         return self.__order
