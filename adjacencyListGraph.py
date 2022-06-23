ac0348899beefd339a5fdbe2f85ce86430d7a348
Thomas Koelman <thomaskoelman@hotmail.com>
2022-06-23 12:47:36 +0200

    Create adjacencyListGraph.py

---
+++ adjacencyListGraph.py
@@ -0,0 +1,38 @@
+class Graph:
+    def __init__(self, directed=False):
+        self.__is_directed = directed
+        self.__storage = []
+        self.__order = 0
+        self.__nmbr_of_edges = 0
+
+    def add_node(self, nmbr=1):
+        for i in range(nmbr):
+            self.__storage.append([])
+            self.__order = self.__order + 1
+
+    def add_edge(self, from_node, to_node):
+        if (not self.is_adjacent(from_node, to_node)):
+            self.__storage[from_node].append(to_node)
+            self.__nmbr_of_edges = self.__nmbr_of_edges + 1
+            if (not self.__is_directed):
+                self.__storage[to_node].append(from_node)
+        else:
+            pass
+
+    def order(self):
+        return self.__order
+
+    def nmbr_of_edges(self):
+        return self.__nmbr_of_edges
+
+    def nodes(self):
+        return set(range(self.order()))
+
+    def neighbours(self, node):
+        return set(self.__storage[node])
+
+    def is_adjacent(self, from_node, to_node):
+        if self.__is_directed:
+            return (to_node in self.__storage[from_node])
+        else:
+            return (to_node in self.__storage[from_node]) and (from_node in self.__storage[to_node])
